d1a385e6baf98f3ceacc42623ba4a5b7045f88e9
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-23 21:55:15 +0900

    add h-full

--- src/components/base/molecules/Header.tsx
+++ src/components/base/molecules/Header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {
 
 export const Header = ({ title, children }: HeaderProps) => {
   return (
-    <header className="flex items-center p-5">
+    <header className="flex h-full items-center p-5">
       <h1 className="select-none text-5xl font-bold text-primary">{title}</h1>
       {children}
     </header>
