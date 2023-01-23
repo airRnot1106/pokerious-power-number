fe242156a4b89fbba2aa96e3b17251085f22ed9c
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-23 22:38:34 +0900

    change text-size

--- src/components/base/molecules/Header.tsx
+++ src/components/base/molecules/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 export const Header = ({ title, children }: HeaderProps) => {
   return (
     <header className="flex h-full items-center p-5">
-      <h1 className="select-none text-5xl font-bold text-primary">
+      <h1 className="select-none text-4xl font-bold text-primary">
         <Link href="/">{title}</Link>
       </h1>
       {children}
