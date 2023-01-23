35d5411811c8ae68efc47dc0a070a335a3c3e2cc
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-23 21:45:48 +0900

    add AppHero

---
+++ src/components/common/site/molecules/AppHero.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+import { Hero } from '@/components/base/molecules/Hero';
+
+export const AppHero = () => {
+  return (
+    <Hero>
+      <h1 className="text-5xl font-bold text-primary">Pokerious</h1>
+      <h2 className="text-2xl">Power-Number</h2>
+      <div className="mt-10 flex flex-col items-center justify-center space-y-5">
+        <Link href="fill-in" className="btn-primary btn">
+          Fill In Mode
+        </Link>
+        <Link href="calc" className="btn-primary btn">
+          Calc Mode
+        </Link>
+      </div>
+    </Hero>
+  );
+};
