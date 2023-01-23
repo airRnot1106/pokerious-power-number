b9cdb81adabc2988b2b59f2a48c7ff69bef931e1
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-23 22:01:54 +0900

    anchor title

--- src/components/base/molecules/Header.tsx
+++ src/components/base/molecules/Header.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 interface HeaderProps {
   title: string;
   children?: React.ReactNode;
@@ -6,7 +8,9 @@ interface HeaderProps {
 export const Header = ({ title, children }: HeaderProps) => {
   return (
     <header className="flex h-full items-center p-5">
-      <h1 className="select-none text-5xl font-bold text-primary">{title}</h1>
+      <h1 className="select-none text-5xl font-bold text-primary">
+        <Link href="/">{title}</Link>
+      </h1>
       {children}
     </header>
   );
