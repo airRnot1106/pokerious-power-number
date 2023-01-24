7f0118979c8c24188126322072ed732d1626eec2
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-24 18:40:42 +0900

    add config for github pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  assetPrefix: isProd ? '/pokerious-power-number' : '',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
