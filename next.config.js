fa420e3be8eaefa26459228cc032c7372db69801
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-24 18:49:25 +0900

    remove config for github pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  assetPrefix: isProd ? '/pokerious-power-number' : '',
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
